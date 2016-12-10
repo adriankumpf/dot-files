f901c496b2f1d5be46134928c8877075a5f9d797
Adrian Kumpf <adrian.kumpf@posteo.de>
2016-12-10 19:52:17 +0100

    Customize install script

--- install.sh
+++ install.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-#----------------------------------------------------------------------------------------
-# Well documented, terminal centric web developer's dot files.
-# Debian/ubuntu based distros installer.
-# @author Martin Toma
-#
-# @version 6.0
-# @updated Sat Nov  7 22:38:01 CET 2015
-# @created Fri Nov 15 13:13:22 CET 2013
-#----------------------------------------------------------------------------------------
-
 # Dont continue on error
 set -e
 
@@ -25,13 +15,6 @@ command_exists() {
   type "$1" &>/dev/null
 }
 
-install_oh_my_zsh() {
-  curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-  ln -sf $current_path/shell/martinus.zsh-theme ~/.oh-my-zsh/themes/martinus.zsh-theme
-  echo "    Change your default shell to zsh"
-  sudo chsh
-}
-
 install_plug_nvim() {
   curl -fLo ~/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 }
@@ -43,53 +26,6 @@ install_nvim_folder() {
   ln -sf $current_path/neovim/UltiSnips ~/.config/nvim/UltiSnips
   ln -sf $current_path/neovim/init.vim ~/.config/nvim/init.vim
 }
-
-#-----------------------------------------------------
-# Basic requirements check
-#-----------------------------------------------------
-
-if ! command_exists apt-get; then
-  echo "This istaller is only comaptible with debian/ubuntu based Linux distributrions."
-  echo "Please install configuration files manually."
-  exit
-fi
-
-if ! command_exists curl; then
-  sudo apt-get install -y curl
-fi
-
-if ! command_exists git; then
-  sudo apt-get install -y git
-fi
-
-#-----------------------------------------------------
-# ZSH installation
-#-----------------------------------------------------
-echo -n "[ zshrc ]"
-
-if [ ! -f ~/.zshrc ]; then
-  echo "    Creating zshrc!"
-  ln -sf $current_path/shell/zshrc ~/.zshrc
-elif $REPLACE_FILES; then
-  echo "    Deleting old zshrc!"
-  rm ~/.zshrc
-  ln -sf $current_path/shell/zshrc ~/.zshrc
-else
-  echo "    Keeping existing zshrc!"
-fi
-
-echo -n "[ oh-my-zsh ]"
-
-if command_exists zsh; then
-  if [ ! -d ~/.oh-my-zsh ]; then
-    echo "    Installing Oh my zsh"
-    install_oh_my_zsh
-  fi
-else
-  echo "    Installing ZSH."
-  sudo apt-get install zsh -y
-  install_oh_my_zsh
-fi
 
 #-----------------------------------------------------
 # Git (config, ignore)
@@ -127,9 +63,7 @@ echo -n "[ Neovim ]"
 
 if ! command_exists nvim; then
   echo "    Installing Neovim!"
-  sudo add-apt-repository ppa:neovim-ppa/unstable
-  sudo apt-get update
-  sudo apt-get install -y neovim
+  brew install neovim/neovim/neovim
 fi
 
 echo -n "[ Neovim config ]"
@@ -147,85 +81,6 @@ else
 fi
 
 #-----------------------------------------------------
-# Installing tmux
-#-----------------------------------------------------
-echo -n "[ tmux.conf ]"
-
-if ! command_exists tmux; then
-  sudo apt-get install tmux -y
-fi
-
-if [ ! -f ~/.tmux.conf ]; then
-  echo "    Creating tmux.conf!"
-  ln -sf $current_path/tmux/tmux.conf ~/.tmux.conf
-elif $REPLACE_FILES; then
-  echo "    Deleting old tmux.conf!"
-  rm ~/.tmux.conf
-  ln -sf $current_path/tmux/tmux.conf ~/.tmux.conf
-else
-  echo "    Keeping existing tmux.conf!"
-fi
-
-#-----------------------------------------------------
-# Installing Xresources
-#-----------------------------------------------------
-echo -n "[ Xresources ]"
-
-if ! command_exists xterm; then
-  sudo apt-get install xterm -y
-fi
-
-if [ ! -f ~/.Xresources ]; then
-  echo "   Creating Xresources!"
-  ln -sf $current_path/shell/Xresources ~/.Xresources
-  xrdb -merge  ~/.Xresources
-elif $REPLACE_FILES; then
-  echo "   Deleting old Xresources!"
-  rm ~/.Xresources
-  ln -sf $current_path/shell/Xresources ~/.Xresources
-  xrdb -merge  ~/.Xresources
-else
-  echo "   Keeping existing Xresources!"
-fi
-
-#-----------------------------------------------------
-# Installing Ruby utilities
-#-----------------------------------------------------
-echo -n "[ Ruby (rbenv) and utilities (gemrc, irbrc, rdebugrc) ]"
-
-if command_exists ruby; then
-  if [ ! -f ~/.gemrc ]; then
-    echo "   Creating gemrc, irbrc, rdebugrc!"
-    ln -sf $current_path/ruby/gemrc ~/.gemrc
-    ln -sf $current_path/ruby/irbrc ~/.irbrc
-    ln -sf $current_path/ruby/rdebugrc ~/.rdebugrc
-  else
-    echo "   Keeping existing gemrc, irbrc, rdebugrc!"
-  fi
-
-  if command_exists pry; then
-    if [ ! -f ~/.pryrc ]; then
-      echo "   Creating pryrc!"
-      ln -sf $current_path/ruby/pryrc ~/.pryrc
-    else
-      echo "   Keeping existing pryrc!"
-    fi
-  else
-    echo "   Installing pry!"
-    gem install pry
-    echo "   Creating pryrc!"
-    ln -sf $current_path/ruby/pryrc ~/.pryrc
-  fi
-else
-  echo "    Installing, rbenv and rubybuild."
-  git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-  git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-  echo "    Restart your shell and install ruby by rbenv install ruby-version"
-  echo "    Then run this script again."
-  exit
-fi
-
-#-----------------------------------------------------
 # Installing Ag
 #-----------------------------------------------------
 echo -n "[ Ag ]"
@@ -239,7 +94,7 @@ if command_exists ag; then
   fi
 else
   echo "   Installing Ag!"
-  sudo apt-get install -y silversearcher-ag
+  brew install the_silver_searcher
   echo "   Creating agignore!"
   ln -sf $current_path/other/agignore ~/.agignore
 fi
@@ -247,34 +102,16 @@ fi
 #-----------------------------------------------------
 # Installing linters
 #-----------------------------------------------------
-echo -n "[ Rubocop ]"
-
-if [ ! -d ~/dev ]; then
-  mkdir ~/dev
-fi
-
-if command_exists rubocop; then
-  ln -sf $current_path/linters/rubocop.yml ~/dev/.rubocop.yml
-else
-  echo "   Installing rubocop!"
-  gem install rubocop
-  ln -sf $current_path/linters/rubocop.yml ~/dev/.rubocop.yml
-fi
-
 echo -n "[ Eslint ]"
 
 if command_exists eslint; then
-  ln -sf $current_path/linters/eslintrc ~/dev/.eslintrc
+  ln -sf $current_path/linters/eslintrc ~/.eslintrc
 else
   if command_exists npm; then
     npm install -g eslint
-    ln -sf $current_path/linters/eslintrc ~/dev/.eslintrc
+    ln -sf $current_path/linters/eslintrc ~/.eslintrc
   else
     echo "   Install node and npm, then rerun script again!"
     exit
   fi
 fi
-
-#-----------------------------------------------------
-# Installing Other stuff
-#-----------------------------------------------------
