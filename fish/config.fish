6f35036a7cdf95247706332acb51196a94edac60
Adrian Kumpf <adrian.kumpf@posteo.de>
2016-12-24 00:19:00 +0100

    Remove outdated paths

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,9 @@ set fish_greeting
 
 ### Program defaults ###
 
-set -x PATH $HOME/.node/bin $PATH
-set -x PATH /usr/local/bin $PATH
-
 set -gx BROWSER open
 set -gx VISUAL nvim
 set -gx EDITOR nvim
 
 set fish_color_command green --bold
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
