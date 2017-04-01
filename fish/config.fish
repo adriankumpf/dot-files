b19e8d0702d05176e383bfb7d69bffd9146eba6e
Adrian Kumpf <adrian.kumpf@posteo.de>
2017-04-01 15:54:29 +0200

    Customize fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -6,5 +6,7 @@ set -gx BROWSER open
 set -gx VISUAL nvim
 set -gx EDITOR nvim
 
-set fish_color_command green --bold
-
+set fish_color_command white --bold
+set pure_color_red (set_color FE6143)
+set pure_color_green (set_color red)
+set pure_color_blue (set_color normal)
