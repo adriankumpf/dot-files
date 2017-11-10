e3282460eb457ddb2ec0da62cf60608707190146
Adrian Kumpf <adrian@peaksandpies.com>
2017-11-10 17:15:55 +0100

    Add helper scripts

--- fish/functions/docker-stop.fish
+++ fish/functions/docker-stop.fish
@@ -1,3 +1,3 @@
-function docker-rm  --wraps "docker stop" --description "Stop all containers"
+function docker-stop  --wraps "docker stop" --description "Stop all containers"
   docker stop (docker ps -a -q)
 end
