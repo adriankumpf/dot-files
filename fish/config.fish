7fe538856b8a86e613bd0f75f1b331f13a365ebd
Adrian Kumpf <adrian.kumpf@posteo.de>
2018-01-28 16:59:10 +0100

    fish: add vim alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,27 @@
 set fish_greeting
+
 set fish_color_command white --bold
 
-set -g fish_user_paths "/usr/local/opt/bison/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/flex/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/go/libexec/bin" $fish_user_paths
+set -g fish_user_paths "$HOME/bin" $fish_user_paths
+
+if type -q cargo
+  set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
+end
+
+if test -x go
+  set -gx GOPATH (go env GOPATH)
+  set -g fish_user_paths "$GOPATH/bin" $fish_user_paths
+  set -g fish_user_paths "/usr/local/opt/go/libexec/bin" $fish_user_paths
+end
+
+if type -q fd
+  set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+  set -gx FZF_DEFAULT_COMMAND "fd --type f --hidden --exclude .git"
+end
 
 set -gx BROWSER "firefox"
 set -gx EDITOR "nvim"
 set -gx ERL_AFLAGS "-kernel shell_history enabled"
-set -gx FZF_DEFAULT_COMMAND "fd --type f --hidden --exclude .git"
-set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 
-
+alias vim="nvim"
