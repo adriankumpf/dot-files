d017bbd670276f7dfed45511b1cf8736a3cb7b37
Adrian Kumpf <adrian.kumpf@posteo.de>
2018-10-26 15:16:40 +0200

    Update fisher to v3

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,12 @@
 set fish_greeting
 
+# Install fisher if necessarry
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
 if [ $ITERM_PROFILE = "light" ]
   set fish_color_command black --bold
 else
