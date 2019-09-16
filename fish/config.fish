fb7fef3fd610200368763d0b83ff5bf91b4fc3fd
Adrian Kumpf <adrian.kumpf@posteo.de>
2019-09-16 20:28:17 +0200

    Remove links to deleted functions

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,12 @@
 set fish_greeting
 
-# Install fisher if necessarry
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
 
-if [ $ITERM_PROFILE = "light" ]
+if [ "$ITERM_PROFILE" = "light" ]
   set fish_color_command black --bold
 else
   set fish_color_command white --bold
@@ -23,12 +22,6 @@ end
 if type -q pip3
   set -x PATH "$HOME/Library/Python/3.7/bin" $PATH
 end
-
-# if type -q go
-#   set -gx GOPATH (`go env GOPATH`)
-#   set -x PATH "$GOPATH/bin" $PATH
-#   set -x PATH "/usr/local/opt/go/libexec/bin" $PATH
-# end
 
 if type -q fd
   set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
