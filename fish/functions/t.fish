2fdc914fc34c758b86f1c11c6731f4683a60684d
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2021-09-27 11:24:04 +0200

    Remove unused fish files

--- fish/functions/t.fish
+++
@@ -1,9 +0,0 @@
-function t --wraps tmux
-  # If provided with args, pass them through.
-  if count $argv > /dev/null
-    tmux $argv
-    # Create (or attach to an existing) session
-  else
-    tmux new -A -s (basename (pwd))
-  end
-end
