4160b9d64c83c3292ff92beaa44da66d208f8abe
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2021-11-30 22:03:21 +0100

    Remove settings that are default in nvim 0.6

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,20 +9,18 @@ vim.opt.clipboard:append {"unnamed"}
 vim.opt.expandtab = true; vim.opt.tabstop = 2; vim.opt.softtabstop = 2; vim.opt.shiftwidth = 2
 vim.opt.fillchars = { vert = "│", stl = " ", stlnc = " " }
 vim.opt.list = true; vim.opt.listchars = { tab = "┆·", trail = '·', precedes = "", extends = "", nbsp = "‗" }; vim.opt.showbreak = "↪"
-vim.opt.joinspaces = false
 vim.opt.number = false; vim.opt.relativenumber = false
 vim.opt.swapfile = false; vim.opt.backup = false; vim.opt.writebackup = false
 vim.opt.undofile = true
 vim.opt.scrolloff = 7
 vim.opt.smartindent = true
 vim.opt.splitbelow = true; vim.opt.splitright = true
-vim.opt.hidden =true; vim.opt.switchbuf = "useopen"
+vim.opt.switchbuf = "useopen"
 vim.opt.termguicolors = true
 vim.opt.mouse = "a"
 vim.opt.signcolumn = "yes"
 vim.opt.showmode = false
 vim.opt.complete:append {"kspell"}
-vim.opt.inccommand = "nosplit"
 vim.opt.wildmode = { list = "longest", list = "full" }
 vim.opt.pumblend = 35
 vim.opt.emoji = false
