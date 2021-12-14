9d0bd470687a0e4d6880fd759ef7268a6d898283
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2021-12-14 11:54:20 +0100

    Fix LuaLine deprecation warning

--- .config/nvim/lua/config/lualine.lua
+++ .config/nvim/lua/config/lualine.lua
@@ -14,6 +14,6 @@ require('lualine').setup({
     theme = 'powerline',
   },
   sections = {
-    lualine_b = {'branch', {'diff', source = diff_source}, {'diagnostics', sources = {'nvim_lsp', 'coc'} } },
+    lualine_b = {'branch', {'diff', source = diff_source}, {'diagnostics', sources = {'nvim_diagnostic', 'coc'} } },
   }
 })
