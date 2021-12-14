68606dc1113fb5b7f424bf6406e808d22edf0642
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2021-12-14 15:33:54 +0100

    Update null-ls config

--- .config/nvim/lua/config/null_ls.lua
+++ .config/nvim/lua/config/null_ls.lua
@@ -1,23 +1,21 @@
 local null_ls = require("null-ls")
 
-local sources = {
-  -- Formatting
-  null_ls.builtins.formatting.shellharden,
-  null_ls.builtins.formatting.prettier,
+null_ls.setup({
+  sources = {
+    -- Formatting
+    null_ls.builtins.formatting.shellharden,
+    null_ls.builtins.formatting.prettier,
 
-  -- Diagnostics
-  null_ls.builtins.diagnostics.hadolint,
-  null_ls.builtins.diagnostics.shellcheck,
-  null_ls.builtins.diagnostics.credo.with {
-    condition = function(utils)
+    -- Diagnostics
+    null_ls.builtins.diagnostics.hadolint,
+    null_ls.builtins.diagnostics.shellcheck,
+    null_ls.builtins.diagnostics.credo.with {
+      condition = function(utils)
         return utils.root_has_file('.credo.exs')
-    end,
+      end,
+    },
+
+    -- Code Actions
+    null_ls.builtins.code_actions.gitsigns,
   },
-
-  -- Code Actions
-  null_ls.builtins.code_actions.gitsigns,
-}
-
-null_ls.config({ sources = sources })
-
-require("lspconfig")["null-ls"].setup({})
+})
