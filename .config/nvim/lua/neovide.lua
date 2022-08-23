f1e6ed83facfe31af7cb4afc57f4555e0c194a26
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2022-08-23 17:12:29 +0200

    Update NeoVide configuration

--- .config/nvim/lua/neovide.lua
+++ .config/nvim/lua/neovide.lua
@@ -1,10 +1,9 @@
 local utils = require 'config.utils' local map = utils.map 
 
 if vim.g.neovide then
-  vim.g.gui_font_face = "FiraCode Nerd Font:h12"
-
   vim.g.neovide_cursor_animation_length = 0.015
-  vim.g.neovide_cursor_trail_length = 0.15
+  vim.g.neovide_cursor_trail_length = 0.01
+  vim.g.neovide_cursor_vfx_mode = "sonicboom"
 
   vim.g.neovide_transparency = 0.0
   vim.g.transparency = 1.0
@@ -13,11 +12,14 @@ if vim.g.neovide then
   vim.g.neovide_input_use_logo=true
   vim.g.neovide_input_macos_alt_is_meta = true
 
-  vim.g.neovide_cursor_vfx_mode = "railgun"
   map('', '<D-v>', '+p<CR>', { silent = true})
   map('!', '<D-v>', '<C-R>+', { silent = true})
   map('t', '<D-v>', '<C-R>+', { silent = true})
   map('v', '<D-v>', '<C-R>+', { silent = true})
+
+  vim.g.gui_font_default_size = 12
+  vim.g.gui_font_size = vim.g.gui_font_default_size
+  vim.g.gui_font_face = "FiraCode Nerd Font"
 
   RefreshGuiFont = function()
     vim.opt.guifont = string.format("%s:h%s",vim.g.gui_font_face, vim.g.gui_font_size)
@@ -38,6 +40,6 @@ if vim.g.neovide then
 
   -- Keymaps
   local opts = { noremap = true, silent = true }
-  vim.keymap.set({'n', 'i'}, "<C-+>", function() ResizeGuiFont(1)  end, opts)
-  vim.keymap.set({'n', 'i'}, "<C-->", function() ResizeGuiFont(-1) end, opts)
+  vim.keymap.set({'n', 'i'}, "<D-=>", function() ResizeGuiFont(1)  end, opts)
+  vim.keymap.set({'n', 'i'}, "<D-->", function() ResizeGuiFont(-1) end, opts)
 end
