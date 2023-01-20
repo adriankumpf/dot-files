cc7b0ffb973b121d98e25068032e9c065b04b4a4
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2023-01-20 13:14:46 +0100

    Add vim-illuminate

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -62,6 +62,20 @@ return {
     }
   },
 
+  -- references
+  {
+    "RRethy/vim-illuminate",
+    event = "BufReadPost",
+    opts = { delay = 200 },
+    config = function(_, opts)
+      require("illuminate").configure(opts)
+    end,
+    keys = {
+      { "]]", function() require("illuminate").goto_next_reference(false) end, desc = "Next Reference", },
+      { "[[", function() require("illuminate").goto_prev_reference(false) end, desc = "Prev Reference" },
+    },
+  },
+
   -- Markdown
   { 'iamcco/markdown-preview.nvim',
     ft = { 'markdown' },
