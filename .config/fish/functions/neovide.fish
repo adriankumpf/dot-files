2a65bebc809ecf1a1ba5cb36bf93eb24f1ab145b
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2023-05-24 00:00:11 +0200

    Remove unused fish function

--- .config/fish/functions/neovide.fish
+++
@@ -1,3 +0,0 @@
-function neovide --wraps='/Applications/Neovide.app/Contents/MacOS/neovide --multigrid' --description 'alias neovide=/Applications/Neovide.app/Contents/MacOS/neovide --multigrid'
-  /Applications/Neovide.app/Contents/MacOS/neovide --multigrid $argv; 
-end
