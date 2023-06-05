82fdab6e9294d1b36da6a48dd5e0d57f0eab3b5e
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2023-06-05 11:26:37 +0200

    Remove unused colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -27,11 +27,6 @@ return {
     end
   },
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = true,
-  },
-  {
     'projekt0n/github-nvim-theme',
     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
