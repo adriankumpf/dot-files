1d4451071ce4a0dcc0e1d19e5bc974de02e91125
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2023-08-09 12:13:53 +0200

    Configure lexical-ls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -24,6 +24,31 @@ return {
       local lsp = require('lsp-zero')
       local cmp = require('cmp')
 
+      local lspconfig = require("lspconfig")
+      local configs = require("lspconfig.configs")
+
+      local lexical_config = {
+        filetypes = { "elixir", "eelixir", },
+        cmd = { vim.loop.os_homedir() .. "/Developer/third_party/lexical/_build/dev/rel/lexical/start_lexical.sh" },
+        settings = {},
+      }
+
+      if not configs.lexical then
+        configs.lexical = {
+          default_config = {
+            filetypes = lexical_config.filetypes,
+            cmd = lexical_config.cmd,
+            root_dir = function(fname)
+              return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
+            end,
+            -- optional settings
+            settings = lexical_config.settings,
+          },
+        }
+      end
+
+      lspconfig.lexical.setup({})
+
       lsp.configure("yamlls", {
         settings = {
           yaml = {
