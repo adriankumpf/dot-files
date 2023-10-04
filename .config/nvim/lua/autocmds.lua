4537eb5a93140b43af570660e77643b90fc39fa4
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2023-10-04 16:16:57 +0200

    Replace null-ls

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -2,65 +2,61 @@
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd("FileType", {
-  pattern = "qf",
-  callback = function()
-    vim.opt_local.buflisted = false
-  end,
+	pattern = "qf",
+	callback = function()
+		vim.opt_local.buflisted = false
+	end,
 })
 
 autocmd("TextYankPost", {
-  pattern = "*",
-  callback = function()
-    require 'vim.highlight'.on_yank()
-  end,
+	pattern = "*",
+	callback = function()
+		require("vim.highlight").on_yank()
+	end,
 })
 
 autocmd("VimEnter", {
-  pattern = "*",
-  callback = function()
-    vim.api.nvim_set_hl(0, 'SignColumn', {})
-  end,
+	pattern = "*",
+	callback = function()
+		vim.api.nvim_set_hl(0, "SignColumn", {})
+	end,
 })
 
 autocmd("VimResized", {
-  pattern = "*",
-  callback = function()
-    vim.cmd [[:wincmd =]]
-  end,
+	pattern = "*",
+	callback = function()
+		vim.cmd([[:wincmd =]])
+	end,
 })
 
 autocmd("BufReadPost", {
-  pattern = { "*" },
-  callback = function()
-    if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
-      vim.api.nvim_exec("normal! g'\"", false)
-    end
-  end
+	pattern = { "*" },
+	callback = function()
+		if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+			vim.api.nvim_exec("normal! g'\"", false)
+		end
+	end,
 })
 
-autocmd('RecordingEnter', {
-  pattern = '*',
-  callback = function()
-    vim.opt_local.cmdheight = 1
-  end,
+autocmd("RecordingEnter", {
+	pattern = "*",
+	callback = function()
+		vim.opt_local.cmdheight = 1
+	end,
 })
 
-autocmd('RecordingLeave', {
-  pattern = '*',
-  callback = function()
-    local timer = vim.loop.new_timer()
-    -- NOTE: Timer is here because we need to close cmdheight AFTER
-    -- the macro is ended, not during the Leave event
-    timer:start(50, 0, vim.schedule_wrap(function()
-      vim.opt_local.cmdheight = 0
-    end)
-    )
-  end,
-})
-
-autocmd("BufWritePre", {
-  pattern = '*',
-  callback = function()
-    pcall(vim.cmd['LspZeroFormat!'])
-  end
+autocmd("RecordingLeave", {
+	pattern = "*",
+	callback = function()
+		local timer = vim.loop.new_timer()
+		-- NOTE: Timer is here because we need to close cmdheight AFTER
+		-- the macro is ended, not during the Leave event
+		timer:start(
+			50,
+			0,
+			vim.schedule_wrap(function()
+				vim.opt_local.cmdheight = 0
+			end)
+		)
+	end,
 })
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,144 +1,179 @@
 return {
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    dependencies = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' },
-      { 'williamboman/mason.nvim',          cmd = "Mason",  build = ":MasonUpdate" },
-      { 'williamboman/mason-lspconfig.nvim' },
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		dependencies = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim", cmd = "Mason", build = ":MasonUpdate" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
 
-      {
-          "Exafunction/codeium.nvim",
-          dependencies = {
-              "nvim-lua/plenary.nvim",
-              "hrsh7th/nvim-cmp",
-          },
-          config = function()
-              require("codeium").setup({
-              })
-          end
-      },
+			{
+				"Exafunction/codeium.nvim",
+				dependencies = {
+					"nvim-lua/plenary.nvim",
+					"hrsh7th/nvim-cmp",
+				},
+				config = function()
+					require("codeium").setup({})
+				end,
+			},
 
-      -- Snippets
-      { 'L3MON4D3/LuaSnip' },
-    },
-    config = function()
-      local lsp_zero = require('lsp-zero')
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+		},
+		config = function()
+			local lsp_zero = require("lsp-zero")
 
-      lsp_zero.on_attach(function(client, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
-        lsp_zero.default_keymaps({buffer = bufnr})
-      end)
+			lsp_zero.on_attach(function(client, bufnr)
+				-- see :help lsp-zero-keybindings
+				-- to learn the available actions
+				lsp_zero.default_keymaps({ buffer = bufnr })
+			end)
 
-      require('mason').setup({})
-      require('mason-lspconfig').setup({
-        ensure_installed = {},
-        handlers = {
-          lsp_zero.default_setup,
-          lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
-          end,
-        }
-      })
+			require("mason").setup({})
+			require("mason-lspconfig").setup({
+				ensure_installed = {},
+				handlers = {
+					lsp_zero.default_setup,
+					lua_ls = function()
+						local lua_opts = lsp_zero.nvim_lua_ls()
+						require("lspconfig").lua_ls.setup(lua_opts)
+					end,
+				},
+			})
 
-      local lsp_configurations = require('lspconfig.configs')
+			local lsp_configurations = require("lspconfig.configs")
 
-      if not lsp_configurations.lexical then
-        lsp_configurations.lexical = {
-          default_config = {
-            name = 'Lexical',
-            filetypes = { "elixir", "eelixir" },
-            cmd = { vim.loop.os_homedir() .. "/Developer/third_party/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
-            root_dir = function(fname)
-              return require('lspconfig.util').root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
-            end,
-            settings = {}
-          }
-        }
-      end
+			if not lsp_configurations.lexical then
+				lsp_configurations.lexical = {
+					default_config = {
+						name = "Lexical",
+						filetypes = { "elixir", "eelixir" },
+						cmd = {
+							vim.loop.os_homedir()
+								.. "/Developer/third_party/lexical/_build/dev/package/lexical/bin/start_lexical.sh",
+						},
+						root_dir = function(fname)
+							return require("lspconfig.util").root_pattern("mix.exs", ".git")(fname)
+								or vim.loop.os_homedir()
+						end,
+						settings = {},
+					},
+				}
+			end
 
-      require('lspconfig').lexical.setup({})
+			require("lspconfig").lexical.setup({})
 
-      require('lspconfig').yamlls.setup({
-        settings = {
-          yaml = {
-            keyOrdering = false
-          }
-        }
-      })
+			require("lspconfig").yamlls.setup({
+				settings = {
+					yaml = {
+						keyOrdering = false,
+					},
+				},
+			})
 
+			local cmp = require("cmp")
+			local cmp_format = lsp_zero.cmp_format()
+			local cmp_action = require("lsp-zero").cmp_action()
 
-      local cmp = require('cmp')
-      local cmp_format = lsp_zero.cmp_format()
-      local cmp_action = require('lsp-zero').cmp_action()
+			cmp.setup({
+				formatting = cmp_format,
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "codeium" },
+					{ name = "buffer" },
+					{ name = "path" },
+				},
+				mapping = cmp.mapping.preset.insert({
+					-- scroll up and down the documentation window
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
 
-      cmp.setup({
-        formatting = cmp_format,
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = "codeium" },
-          { name = 'buffer' },
-          { name = 'path' },
-        },
-        mapping = cmp.mapping.preset.insert({
-          -- scroll up and down the documentation window
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+					-- Use Enter to confirm completion
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
 
-          -- Use Enter to confirm completion
-          ['<CR>'] = cmp.mapping.confirm({ select = false }),
+					-- Enable 'Super Tab'
+					["<Tab>"] = cmp_action.luasnip_supertab(),
+					["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
+				}),
+			})
 
-          -- Enable 'Super Tab'
-          ['<Tab>'] = cmp_action.luasnip_supertab(),
-          ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-        }),
-      })
+			vim.diagnostic.config({
+				virtual_text = true,
+			})
+		end,
+	},
 
-      vim.diagnostic.config({
-        virtual_text = true,
-      })
-    end
-  },
+	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			local lint = require("lint")
 
-  -- Linting, Formatting & Code Actions
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "neovim/nvim-lspconfig"
-    },
-    event = "BufReadPre",
-    opts = function()
-      local null_ls = require("null-ls")
+			lint.linters_by_ft = {
+				elixir = { "credo" },
+				dockerfile = { "hadolint" },
+				sh = { "shellcheck" },
+			}
 
-      return {
-        sources = {
-          -- Formatting
-          null_ls.builtins.formatting.shellharden,
-          null_ls.builtins.formatting.prettier,
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+		end,
+	},
 
-          -- Diagnostics
-          null_ls.builtins.diagnostics.hadolint,
-          null_ls.builtins.diagnostics.shellcheck,
-          null_ls.builtins.diagnostics.credo.with {
-            condition = function(utils)
-              return utils.root_has_file('.credo.exs')
-            end,
-          },
+	{
+		"mhartington/formatter.nvim",
+		config = function()
+			local util = require("formatter.util")
 
-          -- Code Actions
-          null_ls.builtins.code_actions.gitsigns,
-        },
-      }
-    end
-  },
+			require("formatter").setup({
+				filetype = {
+					lua = {
+						require("formatter.filetypes.lua").stylua,
+					},
+
+					fish = {
+						require("formatter.filetypes.fish").fishindent,
+					},
+
+					elixir = {
+						require("formatter.filetypes.elixir").mixformat,
+					},
+
+					sh = {
+						function()
+							return {
+								exe = "shellharden",
+								args = {
+									"--transform",
+									util.escape_path(util.get_current_buffer_file_path()),
+									"--",
+								},
+								stdin = true,
+							}
+						end,
+					},
+
+					-- ["*"] = {
+					--   require("formatter.filetypes.any").remove_trailing_whitespace
+					-- }
+				},
+			})
+
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				callback = function()
+					pcall(vim.cmd["FormatWriteLock"])
+				end,
+			})
+		end,
+	},
 }
