d1afcd121f31ff126a7b9cb1c5b05b37cb8ca968
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2024-02-06 00:13:34 +0100

    Selectively load light colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -31,14 +31,14 @@ return {
 	},
 	{
 		"projekt0n/github-nvim-theme",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
+		lazy = false,
+		priority = 1000,
+		cond = function()
+			return colorscheme() == "github_light"
+		end,
 		config = function()
-			if colorscheme() == "github_light" then
-				require("github-theme").setup({})
-
-				vim.cmd("colorscheme github_light")
-			end
+			require("github-theme").setup({})
+			vim.cmd("colorscheme github_light")
 		end,
 	},
 }
