2c13f54e1a702807b48b7819e7615b414f567a16
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2024-02-08 17:11:41 +0100

    Add support to telescope to preview & jump to line numbers

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -84,6 +84,9 @@ return {
 		config = function()
 			local telescope = require("telescope")
 
+			local actions = require("telescope.actions")
+			local action_state = require("telescope.actions.state")
+
 			local options = {
 				defaults = {
 					vimgrep_arguments = {
@@ -100,6 +103,44 @@ return {
 					set_env = { ["COLORTERM"] = "truecolor" },
 					mappings = {
 						i = { ["<esc>"] = require("telescope.actions").close },
+					},
+				},
+				pickers = {
+					-- Previwe and go to line when searching for "filename:line_number"
+					-- https://github.com/nvim-telescope/telescope.nvim/issues/1042
+					find_files = {
+						on_input_filter_cb = function(prompt)
+							local find_colon = string.find(prompt, ":")
+							if find_colon then
+								local ret = string.sub(prompt, 1, find_colon - 1)
+								vim.schedule(function()
+									local prompt_bufnr = vim.api.nvim_get_current_buf()
+									local picker = action_state.get_current_picker(prompt_bufnr)
+									local lnum = tonumber(prompt:sub(find_colon + 1))
+									if type(lnum) == "number" then
+										local win = picker.previewer.state.winid
+										local bufnr = picker.previewer.state.bufnr
+										local line_count = vim.api.nvim_buf_line_count(bufnr)
+										vim.api.nvim_win_set_cursor(win, { math.max(1, math.min(lnum, line_count)), 0 })
+									end
+								end)
+								return { prompt = ret }
+							end
+						end,
+						attach_mappings = function()
+							actions.select_default:enhance({
+								post = function()
+									-- if we found something, go to line
+									local prompt = action_state.get_current_line()
+									local find_colon = string.find(prompt, ":")
+									if find_colon then
+										local lnum = tonumber(prompt:sub(find_colon + 1))
+										vim.api.nvim_win_set_cursor(0, { lnum, 0 })
+									end
+								end,
+							})
+							return true
+						end,
 					},
 				},
 			}
